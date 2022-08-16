12e215f895d91b9fdb2f93591a19ac3152c91a80
ZeljkoCarevic <zeljkocarevic98@gmail.com>
2022-08-16 20:33:57 +0200

    added LS component

--- src/comps/Popular.jsx
+++ src/comps/Popular.jsx
@@ -11,11 +11,17 @@ function Popular() {
   }, []);
 
   const getPopular = async () => {
-    const res = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_KEY}&number=9`
-    );
-    const data = await res.json();
-    setPopular(data.recipes);
+    const check = localStorage.getItem("popular");
+    if (check) {
+      setPopular(JSON.parse(check));
+    } else {
+      const res = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_KEY}&number=9`
+      );
+      const data = await res.json();
+      localStorage.setItem("popular", JSON.stringify(data.recipes));
+      setPopular(data.recipes);
+    }
   };
 
   return (
@@ -23,13 +29,21 @@ function Popular() {
       <Wrapper>
         <h3>Popular pics</h3>
 
-        <Splide>
+        <Splide
+          options={{
+            perPage: 3,
+            arrows: false,
+            drag: "free",
+            gap: "5rem",
+          }}
+        >
           {popular.map((recipe) => {
             return (
               <SplideSlide>
                 <Card>
                   <p>{recipe.title}</p>
                   <img src={recipe.image} alt="" />
+                  <gradient />
                 </Card>
               </SplideSlide>
             );
@@ -48,9 +62,38 @@ const Card = styled.div`
   min-height: 25rem;
   border-radius: 2rem;
   overflow: hidden;
+  position: relative;
   img {
     border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 500;
+    font-size: 1.5rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 `;
 
+const gradient = styled.div`
+  z-index: 3;
+  position: aboslute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+`;
 export default Popular;
