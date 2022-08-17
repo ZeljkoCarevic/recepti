5cd1eea5c2afbeb313b66ce2457a8d59716fe2b6
ZeljkoCarevic <zeljkocarevic98@gmail.com>
2022-08-17 16:09:28 +0200

    added the search function, gotta add the recipes

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Home from "./Home";
 import { Route, Routes } from "react-router-dom";
 import Cousine from "./Cousine";
+import Searched from "./Searched";
 
 function Pages() {
   return (
     <Routes>
       <Route path="/" element={<Home></Home>}></Route>
       <Route path="/Cousine/:type" element={<Cousine></Cousine>}></Route>
+      <Route path="/searched/:search" element={<Searched />}></Route>
     </Routes>
   );
 }
